70241556d897450ad631bd631ed59eb69ab8ab28
Blair Peterson and Kyle Derrick <kderrick1988@gmail.com>
2016-02-08 13:27:42 -0800

    accept user input and return pingpong array

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,13 +1,44 @@
 import java.util.*;
+import static spark.Spark.*;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
 
 public class PingPong {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
 
-    public ArrayList countPingPong(Integer userNumber) {
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/index.vtl" );
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/results", (request, response) -> {
+      HashMap model = new HashMap();
+      int userNumber = Integer.parseInt(request.queryParams("userNumber"));
+
+      ArrayList <Object> numbersArray = PingPong.countPingPong(userNumber);
+      model.put("numbersArray", numbersArray);
+      model.put("userNumber", userNumber);
+      model.put("template", "templates/results.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+}
+
+
+
+
+    public static ArrayList countPingPong(Integer userNumber) {
       ArrayList <Object> numbersArray = new ArrayList<Object>();
       for ( Integer index = 1; index <= userNumber; index++) {
-        if (index % 3 == 0) {
+        if (index % 15 == 0) {
+          numbersArray.add("ping pong");
+        } else if (index % 3 == 0) {
           numbersArray.add("ping");
+        } else if (index % 5 == 0) {
+          numbersArray.add("pong");
         } else {
           numbersArray.add(index);
         }
--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -23,4 +23,38 @@ public class PingPongTest {
     assertEquals(numbersArray, pingPong.countPingPong(3));
   }
 
+  @Test
+  public void countPingPong_replaceFivesWithPong() {
+    PingPong pingPong = new PingPong();
+    ArrayList<Object> numbersArray = new ArrayList<Object>();
+    numbersArray.add(1);
+    numbersArray.add(2);
+    numbersArray.add("ping");
+    numbersArray.add(4);
+    numbersArray.add("pong");
+    assertEquals(numbersArray, pingPong.countPingPong(5));
+  }
+
+  @Test
+  public void countPingPong_replaceMultiplesOfFifteen() {
+    PingPong pingPong = new PingPong();
+    ArrayList<Object> numbersArray = new ArrayList<Object>();
+    numbersArray.add(1);
+    numbersArray.add(2);
+    numbersArray.add("ping");
+    numbersArray.add(4);
+    numbersArray.add("pong");
+    numbersArray.add("ping");
+    numbersArray.add(7);
+    numbersArray.add(8);
+    numbersArray.add("ping");
+    numbersArray.add("pong");
+    numbersArray.add(11);
+    numbersArray.add("ping");
+    numbersArray.add(13);
+    numbersArray.add(14);
+    numbersArray.add("ping pong");
+    assertEquals(numbersArray, pingPong.countPingPong(15));
+  }
+
 }
