ced893ebafa66b9cc5d1791904b795dc834a25bb
Blair Peterson and Kyle Derrick <kderrick1988@gmail.com>
2016-02-08 10:31:51 -0800

    add replace 3 with ping and passing spec

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -4,10 +4,14 @@ public class PingPong {
   public static void main(String[] args) {}
 
     public ArrayList countPingPong(Integer userNumber) {
-      ArrayList <Integer> numbersArray = new ArrayList<Integer>();
-    for ( Integer index = 1; index <= userNumber; index++) {
-       numbersArray.add(index);
+      ArrayList <Object> numbersArray = new ArrayList<Object>();
+      for ( Integer index = 1; index <= userNumber; index++) {
+        if (index % 3 == 0) {
+          numbersArray.add("ping");
+        } else {
+          numbersArray.add(index);
+        }
       }
       return numbersArray;
-    }
+  }
 }
--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -7,9 +7,20 @@ public class PingPongTest {
   @Test
   public void countPingPong_countUpToNumberFromUser() {
     PingPong pingPong = new PingPong();
-    ArrayList <Integer> numbersArray = new ArrayList<Integer>();
+    ArrayList<Object> numbersArray = new ArrayList<Object>();
     numbersArray.add(1);
     numbersArray.add(2);
     assertEquals(numbersArray, pingPong.countPingPong(2));
   }
+
+  @Test
+  public void countPingPong_replaceThreesWithPing() {
+    PingPong pingPong = new PingPong();
+    ArrayList<Object> numbersArray = new ArrayList<Object>();
+    numbersArray.add(1);
+    numbersArray.add(2);
+    numbersArray.add("ping");
+    assertEquals(numbersArray, pingPong.countPingPong(3));
+  }
+
 }
